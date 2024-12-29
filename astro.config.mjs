189832c07fabbf91d1e4c695cd604c2e1b577f10
Cristian escobar <162354998+crisescobaro@users.noreply.github.com>
2024-12-29 15:01:21 -0500

    probando de nuevo <3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,38 @@
+// import react from "@astrojs/react";
+// import tailwind from "@astrojs/tailwind";
+// import { defineConfig } from "astro/config";
+
+// // https://astro.build/config
+// export default defineConfig({
+//   integrations: [tailwind(), react()],
+//   i18n: {
+//     defaultLocale: "es",
+//     locales: ["es", "en", "fr"],
+//     routing: {
+//       prefixDefaultLocale: false,
+//     },
+//     // fallback: {
+//     //   en: "es"
+//     // }
+//   },
+// });
+
+
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel"; // Importa el adaptador de Vercel
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
+  output: "server", // Mantén el modo de salida como "server"
   integrations: [tailwind(), react()],
+  adapter: vercel(), // Configura el adaptador de Vercel
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en", "fr"],
     routing: {
       prefixDefaultLocale: false,
     },
-    // fallback: {
-    //   en: "es"
-    // }
   },
 });
+
